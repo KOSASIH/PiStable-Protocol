91995277c23993df1dc782589f50b8130dee9de1
KOSASIH <kosasihg88@gmail.com>
2025-01-03 09:37:19 +0700

    Create constants.js

---
+++ scripts/utils/constants.js
@@ -0,0 +1,22 @@
+// scripts/utils/constants.js
+
+const TOKEN_DECIMALS = 18;
+
+const ADDRESSES = {
+    PI_STABLE: "0xYourPiStableAddress",
+    GOVERNANCE: "0xYourGovernanceAddress",
+    COLLATERAL_MANAGER: "0xYourCollateralManagerAddress",
+    PRICE_ORACLE: "0xYourPriceOracleAddress",
+    STAKING: "0xYourStakingAddress",
+    LIQUIDITY_POOL: "0xYourLiquidityPoolAddress",
+    MULTI_SIG_WALLET: "0xYourMultiSigWalletAddress",
+    ESCROW: "0xYourEscrowAddress",
+};
+
+const REWARD_RATE = "1"; // Example reward rate for staking
+
+module.exports = {
+    TOKEN_DECIMALS,
+    ADDRESSES,
+    REWARD_RATE,
+};
