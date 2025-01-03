2f8fe820089d4208963d220e1d38adfe7ceae98c
KOSASIH <kosasihg88@gmail.com>
2025-01-03 13:23:32 +0700

    Update PiStable.sol

--- contracts/PiStable.sol
+++ contracts/PiStable.sol
@@ -3,40 +3,33 @@ pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
 
-contract PiStable is ERC20, Ownable, Pausable {
-    uint256 public constant INITIAL_SUPPLY = 1_000_000 * 10**18; // 1 million tokens
-    uint256 public pegValue = 314159; // Peg value in cents (i.e., $314.159)
+contract PiStable is ERC20, Ownable {
+    // The value of the stablecoin in USD (pegged to $314.159)
+    uint256 public constant STABLE_COIN_VALUE = 314159000000000000000; // 314.159 in wei (1e18 = 1 token)
+    uint256 public constant TOTAL_SUPPLY = 200000000000 * (10 ** decimals()); // 200 billion tokens
 
-    event PegValueUpdated(uint256 newPegValue);
+    // Event to emit when the value is updated
+    event ValueUpdated(uint256 newValue);
 
     constructor() ERC20("PiStable", "PST") {
-        _mint(msg.sender, INITIAL_SUPPLY);
+        _mint(msg.sender, TOTAL_SUPPLY); // Mint total supply to the owner
     }
 
-    function mint(address to, uint256 amount) external onlyOwner whenNotPaused {
-        _mint(to, amount);
+    // Function to get the current value of the stablecoin
+    function getStableCoinValue() external pure returns (uint256) {
+        return STABLE_COIN_VALUE;
     }
 
-    function burn(uint256 amount) external whenNotPaused {
-        _burn(msg.sender, amount);
+    // Function to buy stablecoins
+    function buyStableCoins() external payable {
+        require(msg.value > 0, "Send ETH to buy stablecoins");
+        uint256 amountToMint = (msg.value * STABLE_COIN_VALUE) / 1 ether; // Calculate amount based on the stablecoin value
+        _mint(msg.sender, amountToMint);
     }
 
-    function setPegValue(uint256 newPegValue) external onlyOwner {
-        pegValue = newPegValue;
-        emit PegValueUpdated(newPegValue);
-    }
-
-    function getPegValue() external view returns (uint256) {
-        return pegValue;
-    }
-
-    function pause() external onlyOwner {
-        _pause();
-    }
-
-    function unpause() external onlyOwner {
-        _unpause();
+    // Function to withdraw ETH from the contract
+    function withdraw(uint256 amount) external onlyOwner {
+        payable(owner()).transfer(amount);
     }
 }
