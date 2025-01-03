736018f254be431e29a29588537e6da39d6d8a34
KOSASIH <kosasihg88@gmail.com>
2025-01-03 10:20:51 +0700

    Create Liquidity.js

---
+++ src/pages/Liquidity.js
@@ -0,0 +1,16 @@
+// src/pages/Liquidity.js
+import React from 'react';
+import './Liquidity.css';
+
+const Liquidity = () => {
+    return (
+        <div className="liquidity">
+            <h2>Liquidity Management</h2>
+            <p>Provide liquidity to earn fees and participate in the DeFi ecosystem.</p>
+            {/* Add components for adding/removing liquidity here */}
+            <p>Coming soon: Add and remove liquidity functionality!</p>
+        </div>
+    );
+};
+
+export default Liquidity;
