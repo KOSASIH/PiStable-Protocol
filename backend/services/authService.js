a963393894cd66b32c9c116ef66c496f3987b782
KOSASIH <kosasihg88@gmail.com>
2025-01-03 10:47:20 +0700

    Create authService.js

---
+++ backend/services/authService.js
@@ -0,0 +1,24 @@
+// backend/services/authService.js
+const User = require('../database/models/User');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const register = async ({ username, password, email }) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ username, password: hashedPassword, email });
+    await user.save();
+    return user;
+};
+
+const login = async ({ username, password }) => {
+    const user = await User.findOne({ username });
+    if (!user) throw new Error('User  not found');
+    
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new Error('Invalid credentials');
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+    return token;
+};
+
+module.exports = { register, login };
